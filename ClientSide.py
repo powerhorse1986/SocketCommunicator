94665ab80cffa6708bb97cd4df99a779cb2839e5
powerhorse1986@gmail.com <powerhorse1986@gmail.com>
2023-06-21 13:18:39 -0500

    Wrap the code into classes

--- ClientSide.py
+++ ClientSide.py
@@ -8,102 +8,137 @@ import socket
 import selectors
 import types
 
-# The server's hostname or IP address
-# HOST = input("Type in the IP address of the server")
-HOST = ""
-
-# The port used by the server
-PORT = 9898
-
-sel = selectors.DefaultSelector()
-messages = [b"Message 1 from client.", b"Message 2 from client."]
-
-def start_connection(host, port, num_conns = len(messages)):
-    """Initiate a connection to the server
-
-    Parameters
-    ----------
-    host : str
-        The name of the host or the IP address of the host
-    port : int
-        The port number to which the server be connected
-    num_conns : int        Number of connections created
+class Client:
     """
-    
-    server_addr = (host, port)
-    for i in range(num_conns):
-        conn_id = i + 1
-        print(f"Starting connection {conn_id} to {host, port}")
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.setblocking(False)
-        sock.connect_ex(server_addr)
-        events = selectors.EVENT_READ | selectors.EVENT_WRITE
-        data = types.SimpleNamespace(
-            conn_id = conn_id,
-            msg_total = sum(len(m) for m in messages),
-            recv_total = 0,
-            messages = messages.copy(),
-            outb = b""
-        )
-        sel.register(sock, events, data = data)
+    A class used to represent the client side of the client-server program
 
-def service_connection(key, mask):
-    """This function connects to the server socket
-
-    Parameters
-    ----------
-    key : The SelectorKey instance corresponding to a ready file object.
-        key contains the socket object (fileobj) and data object
-    mask : A bit mask
-        a bitmask of events ready on this file object
+    Methods
+    -------
+    start_connection(host, port, num_conns)
+        Initiate the connection to the server
+    service_connection(key, mask)
+        Process the connection with the server
     """
 
-    # The socket object 
-    sock = key.fileobj
-
-    # The data object
-    data = key.data
-
-    if mask & selectors.EVENT_READ:
-        # Take out the data transferred by the socket
-        recv_data = sock.recv(1024)
-
-        # If we receive data which is not empty, print it out and update
-        # the data.recv_total
-        if recv_data:
-            print(f"Received {recv_data} from connection {data.conn_id}")
-            data.recv_total += len(recv_data)
-
-        # If the received data is empty or data.recv_total == data.msg_total
-        # that means the connection should be closed
-        if not recv_data or data.recv_total == data.msg_total:
-            print(f"Closing connection {data.conn_id}")
-            sel.unregister(sock)
-            sock.close()
-
-    if mask & selectors.EVENT_WRITE:
-        # If the data.outb is empty, we pop out the contents in data.message
-        if not data.outb and data.messages:
-            data.outb = data.messages.pop(0)
+    def __init__(self) -> None:
+        self.sel = selectors.DefaultSelector()
+        self.messages = []
         
-        # If the data.outb is not empty, we send it out and trim off the buffer
-        if data.outb:
-            print(f"Sending {data.outb!r} to connection {data.conn_id}")
-            sent = sock.send(data.outb)
-            data.outb = data.outb[sent : ]
+    def set_msg(self, msg) -> None:
+        """This function sets up the message sending to the server for 
+        echoing
 
-start_connection(HOST, PORT)
+        Parameters
+        ----------
+        msg : list
+            A list contains the messages
+        """
+        self.messages.append(msg)
 
-try:
-    while True:
-        events = sel.select(timeout = 1)
-        if events:
-            for key, mask in events:
-                service_connection(key, mask)
-        # Check for a socket being monitored to continue
-        if not sel.get_map():
-            break
-except KeyboardInterrupt:
-    print("Caught keyboard intterrupt, exiting")
-finally:
-    sel.close()
+
+    def start_connection(self, host, port, num_conns) -> None:
+        """Initiate a connection to the server
+
+        Parameters
+        ----------
+        host : str
+            The name of the host or the IP address of the host
+        port : int
+            The port number to which the server be connected
+        num_conns : int        Number of connections created
+        """
+        
+        server_addr = (host, port)
+        for i in range(num_conns):
+            conn_id = i + 1
+            print(f"Starting connection {conn_id} to {server_addr}")
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.setblocking(False)
+            sock.connect_ex(server_addr)
+            events = selectors.EVENT_READ | selectors.EVENT_WRITE
+            data = types.SimpleNamespace(
+                conn_id = conn_id,
+                msg_total = sum(len(m) for m in self.messages),
+                recv_total = 0,
+                messages = self.messages.copy(),
+                outb = b""
+            )
+            self.sel.register(sock, events, data = data)
+
+    def service_connection(self, key, mask) -> None:
+        """This function connects to the server socket
+
+        Parameters
+        ----------
+        key : The SelectorKey instance corresponding to a ready file object.
+            key contains the socket object (fileobj) and data object
+        mask : A bit mask
+            a bitmask of events ready on this file object
+        """
+
+        # The socket object 
+        sock = key.fileobj
+
+        # The data object
+        data = key.data
+
+        if mask & selectors.EVENT_READ:
+            # Take out the data transferred by the socket
+            recv_data = sock.recv(1024)
+
+            # If we receive data which is not empty, print it out and update
+            # the data.recv_total
+            if recv_data:
+                print(f"Received {recv_data} from connection {data.conn_id}")
+                data.recv_total += len(recv_data)
+
+            # If the received data is empty or data.recv_total == data.msg_total
+            # that means the connection should be closed
+            if not recv_data or data.recv_total == data.msg_total:
+                print(f"Closing connection {data.conn_id}")
+                self.sel.unregister(sock)
+                sock.close()
+
+        if mask & selectors.EVENT_WRITE:
+            # If the data.outb is empty, we pop out the contents in data.message
+            if not data.outb and data.messages:
+                data.outb = data.messages.pop(0)
+            
+            # If the data.outb is not empty, we send it out and trim off the buffer
+            if data.outb:
+                print(f"Sending {data.outb!r} to connection {data.conn_id}")
+                sent = sock.send(data.outb)
+                data.outb = data.outb[sent : ]
+    
+    def main(self):
+        # The server's hostname or IP address
+        # HOST = input("Type in the IP address of the server")
+        HOST = ""
+
+        # The port used by the server
+        PORT = 9898
+
+        messages = [b"Message 1 from client.", b"Message 2 from client."]
+        self.set_msg(messages)
+        self.start_connection(HOST, PORT, len(messages))
+
+        try:
+            while True:
+                events = self.sel.select(timeout = 1)
+                if events:
+                    for key, mask in events:
+                        self.service_connection(key, mask)
+                # Check for a socket being monitored to continue
+                if not self.sel.get_map():
+                    break
+        except KeyboardInterrupt:
+            print("Caught keyboard intterrupt, exiting")
+        finally:
+            self.sel.close()
+
+if __name__ == "__main__":
+    client = Client()
+    client.main()
+
+
+        
--- ServerSide.py
+++ ServerSide.py
@@ -10,140 +10,156 @@ import socket
 import selectors
 import types
 
-# Standard loopback interface address (localhost)
-HOST = "0.0.0.0"
-# Port to listen on
-PORT = 9898
-
-# The selectors module allows high-level and efficient I/O
-# multiplexing. DefaultSelector is an alias to the most 
-# effecient implementation available on the current platform.
-selector = selectors.DefaultSelector()
-
-def accept_wrapper(sock):
-    """This function gets the new socket object and register it with the 
-    selector
-
-    Parameters
-    ----------
-    sock : a fileobj of the key in the tuple returned by selector.select()
-        The fileobj of the key is the file boject registered. Till now we 
-        only registered the sock listening on (HOST, PORT)
+class Server:
     """
-    # Because the listening block was registered for the event 
-    # selectors.EVENT_READ, it should be ready to readl. Call sock.accept()
-    # and then call conn.setblocking(False) to put the socket in non-blocking
-    # mode
-    conn, addr = sock.accept()
-    print(f"Accepted connection from {addr}")
-    conn.setblocking(False)
+    A class used to represent the server side of the client-server program
 
-    # Create an object to hold the data that we want to included along with 
-    # the socket using a SimpleNamespace.
-    data = types.SimpleNamespace(addr = addr, inb = b"", outb = b"")
-
-    # We want to know when the client connection is ready for reading and 
-    # writing
-    events = selectors.EVENT_READ | selectors.EVENT_WRITE
-
-    selector.register(conn, events, data = data)
-
-def service_connection(key, mask):
-    """This function services the client socket
-
-    Parameters
-    ----------
-    key : The SelectorKey instance corresponding to a ready file object.
-        key contains the socket object (fileobj) and data object
-    mask : A bit mask
-        a bitmask of events ready on this file object
+    Methods
+    -------
+    accept_wrapper(sock)
+        Gets the new socket object and register it with the selector
+    service_connection(key, mask)
+        Services the client socket
     """
-    sock = key.fileobj
-    data = key.data
 
-    # If the socket is ready for reading then mask & selectors.EVENT_READ
-    # will be True, thus sock.recv() is called. Any data that's read is 
-    # appended to data.outb so that it can be sent later
-    if mask & selectors.EVENT_READ:
-        recv_data = sock.recv(1024)
-        print(f"Data: {data}")
-        if recv_data:
-            print(f"Data: {recv_data!r}")
-            data.outb += recv_data
-        # If no data is received, this means that the client has closed
-        # its socket, so the server should too
-        else:
-            print(f"Closing connection to {data.addr}")
-            selector.unregister(sock)
-            sock.close()
+    def __init__(self) -> None:
+        # socket.AF_INET: The internet address family of IPv4
+        # socket.SOCK_STREAM: The socket type for TCP
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # The selectors module allows high-level and efficient I/O
+        # multiplexing. DefaultSelector is an alias to the most 
+        # effecient implementation available on the current platform.
+        self.selector = selectors.DefaultSelector()
 
-    # When the socket is ready for writing, which should always be the case
-    # for a healthy socket, any received data stored in data.outb is echoed
-    # to the client using sock.send(). The bytes sent are then removed from
-    # the send buffer.
-    if mask & selectors.EVENT_WRITE:
-        if data.outb:
-            print(f"Closing connection to {data.addr}")
+    def accept_wrapper(self, sock) -> None:
+        """This function gets the new socket object and register it with the 
+        selector
 
-            # The .send() method returns the number of bytes sent. This 
-            # number then be used to slice the .outb buffer to discard the
-            # bytes sent later
-            try:
-                sent = sock.send(data.outb)
-                data.oub = data.outb[sent : ]
-            except Exception as e:
-                print(e)
+        Parameters
+        ----------
+        sock : a fileobj of the key in the tuple returned by selector.select()
+            The fileobj of the key is the file boject registered. Till now we 
+            only registered the sock listening on (HOST, PORT)
+        """
+        # Because the listening block was registered for the event 
+        # selectors.EVENT_READ, it should be ready to readl. Call sock.accept()
+        # and then call conn.setblocking(False) to put the socket in non-blocking
+        # mode
+        conn, addr = sock.accept()
+        print(f"Accepted connection from {addr}")
+        conn.setblocking(False)
 
+        # Create an object to hold the data that we want to included along with 
+        # the socket using a SimpleNamespace.
+        data = types.SimpleNamespace(addr = addr, inb = b"", outb = b"")
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-    """
-    socket.AF_INET: The internet address family of IPv4
-    socket.SOCK_STREAM: The socket type for TCP
-    """
-    sock.bind((HOST, PORT))
-    sock.listen()
-    print(f"Listening on {HOST, PORT}")
+        # We want to know when the client connection is ready for reading and 
+        # writing
+        events = selectors.EVENT_READ | selectors.EVENT_WRITE
 
-    # .setblocking(False) configures the socket in non-blocking
-    # mode. Calls made to this socket will no longer block.
-    sock.setblocking(False)
+        self.selector.register(conn, events, data = data)
 
-    # selector.register() registers the socket to be monitored
-    # with selector.register() for the event that we are 
-    # interested in. For the listening socket, we want read 
-    # events: selectors.EVENT_READ
-    selector.register(sock, selectors.EVENT_READ, data = None)
-    
-    try:
+    def service_connection(self, key, mask) -> None:
+        """This function services the client socket
+
+        Parameters
+        ----------
+        key : The SelectorKey instance corresponding to a ready file object.
+            key contains the socket object (fileobj) and data object
+        mask : A bit mask
+            a bitmask of events ready on this file object
+        """
+        sock = key.fileobj
+        data = key.data
+
+        # If the socket is ready for reading then mask & selectors.EVENT_READ
+        # will be True, thus sock.recv() is called. Any data that's read is 
+        # appended to data.outb so that it can be sent later
+        if mask & selectors.EVENT_READ:
+            recv_data = sock.recv(1024)
+            print(f"Data: {data}")
+            if recv_data:
+                print(f"Data: {recv_data!r}")
+                data.outb += recv_data
+            # If no data is received, this means that the client has closed
+            # its socket, so the server should too
+            else:
+                print(f"Closing connection to {data.addr}")
+                self.selector.unregister(sock)
+                sock.close()
+
+        # When the socket is ready for writing, which should always be the case
+        # for a healthy socket, any received data stored in data.outb is echoed
+        # to the client using sock.send(). The bytes sent are then removed from
+        # the send buffer.
+        if mask & selectors.EVENT_WRITE:
+            if data.outb:
+                print(f"Closing connection to {data.addr}")
+
+                # The .send() method returns the number of bytes sent. This 
+                # number then be used to slice the .outb buffer to discard the
+                # bytes sent later
+                try:
+                    sent = sock.send(data.outb)
+                    data.oub = data.outb[sent : ]
+                except Exception as e:
+                    print(e)
+
+    def main(self):
+        # Standard loopback interface address (localhost)
+        HOST = "0.0.0.0"
+        # Port to listen on
+        PORT = 9898
+
+        self.sock.bind((HOST, PORT))
+        self.sock.listen()
+        print(f"Listening on {HOST, PORT}")
+
+        # .setblocking(False) configures the socket in non-blocking
+        # mode. Calls made to this socket will no longer block.
+        self.sock.setblocking(False)
+
+        # selector.register() registers the socket to be monitored
+        # with selector.register() for the event that we are 
+        # interested in. For the listening socket, we want read 
+        # events: selectors.EVENT_READ
+        self.selector.register(self.sock, selectors.EVENT_READ, data = None)
         
-        while True:
+        try:
             
-            # selector.select(timeout = None) blocks until there
-            # are sockets ready for I/O. It returns a list of 
-            # tuples, one for each socket. Each tuple contains 
-            # a key and a mask. They key is the SelectorKey 
-            # instance corresponding to a ready file object. key.fileobj
-            # is the socke object. mask is a bitmask of events
-            #  ready on this file object
-            events = selector.select(timeout = None)
-            
-
-            for key, mask in events:
-
-                # If key.data is None, then we know it's from the
-                # listening socket and we need to accept the connection.
-                # We'll call the accept_wrapper() function to get the 
-                # new socket object and register it with the selector
-                if key.data is None:
-                    accept_wrapper(key.fileobj)
+            while True:
                 
-                # If key.data is not None, we know it's a client socket
-                # that's ready been accepted, and we need to service it
-                # The function service_connection() is then called with 
-                # key and mask as arguments
-                else:
-                    service_connection(key, mask)
-    except KeyboardInterrupt:
-        print("Cauth keyboard interrupt, exiting")
-    finally:
-        selector.close()
+                # selector.select(timeout = None) blocks until there
+                # are sockets ready for I/O. It returns a list of 
+                # tuples, one for each socket. Each tuple contains 
+                # a key and a mask. They key is the SelectorKey 
+                # instance corresponding to a ready file object. key.fileobj
+                # is the socke object. mask is a bitmask of events
+                #  ready on this file object
+                events = self.selector.select(timeout = None)
+                
+
+                for key, mask in events:
+
+                    # If key.data is None, then we know it's from the
+                    # listening socket and we need to accept the connection.
+                    # We'll call the accept_wrapper() function to get the 
+                    # new socket object and register it with the selector
+                    if key.data is None:
+                        self.accept_wrapper(key.fileobj)
+                    
+                    # If key.data is not None, we know it's a client socket
+                    # that's ready been accepted, and we need to service it
+                    # The function service_connection() is then called with 
+                    # key and mask as arguments
+                    else:
+                        self.service_connection(key, mask)
+        except KeyboardInterrupt:
+            print("Cauth keyboard interrupt, exiting")
+        finally:
+            self.sock.close()
+            self.selector.close()
+
+if __name__ == "__main__":
+    server = Server()
+    server.main()
